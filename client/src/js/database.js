54a31b6ef5b574fc41fce010a5792f64b3a9c2e7
Olive Provencio-Johnson <120212494+Olive-Provencio-Johnson@users.noreply.github.com>
2023-06-07 02:54:22 -0700

    edited database.js, including get, post and put methods

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,42 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Added logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({content});
+  const result = await request;
+  console.log('Data updated', result);
+  return result;
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+
+
+// Added logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log('Successful GET from database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
+
+
+
+// Added logic for a method that posts content to the database
+export const postDb = async (content) => {
+  console.log('Successful POST to database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.add({content});
+  const result = await request;
+  console.log('New data added to database', result);
+};
 
 initdb();
