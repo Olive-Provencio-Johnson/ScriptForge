422627f90c685e885f6e850d1e15ae4de4a5de5e
Olive Provencio-Johnson <120212494+Olive-Provencio-Johnson@users.noreply.github.com>
2023-06-16 15:16:47 -0700

    webpack.config.js file module rules

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -25,6 +25,21 @@ module.exports = () => {
 
     module: {
       rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader', 
+            options: {
+              presets: ['@babel/preset-env'], 
+              plugins: ['@babel/plugin-proposal-object-rest-spread'], 
+            }
+          }
+        }
         
       ],
     },
